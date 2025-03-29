c37c4162ce4f3d1ea883408e75cb5a99f6316ba4
makashinmi <mixanidze3521@gmail.com>
2025-03-29 10:56:03 +0300

    encryption: Revision in accordance with AA feedback

--- src/cryptor.h
+++ src/cryptor.h
@@ -1,3 +1,6 @@
+#ifndef CRYPTOR_H_
+#define CRYPTOR_H_
+
 #include <filesystem>
 #include <iostream>
 #include <openssl/evp.h>
@@ -6,30 +9,30 @@
 class AES256CBC {
     public:
         bool isLogging = true;
-        std::ostream nullStream{nullptr};
 
-        std::ostream& cout() { return isLogging ? std::cout : nullStream; };
-        std::ostream& cerr() { return isLogging ? std::cerr : nullStream; };
         bool decryptFile(std::filesystem::path path);
-        bool decryptRecursively(const std::string path);
-        bool encryptFile(const std::filesystem::path pathIn);
-        bool encryptRecursively(const std::string path);
-        void keyFromFile();
+        bool decryptRecursively(const std::filesystem::path& path);
+        bool encryptFile(const std::filesystem::path& pathIn);
+        bool encryptRecursively(const std::filesystem::path& path);
         void generateKey();
-        void keyToFile();
+        void keyFromFile();
 
         AES256CBC();
-        AES256CBC(unsigned char* key);
         ~AES256CBC();
 
-    protected:
-        const int keySize = 32;
-        const int blockSize = 16;
-        const std::string extension = ".hackademic";
+    private:
+        const int keySize_ = 32;
+        const int blockSize_ = 16;
+        const std::string extension_ = ".hackademic";
         unsigned char* key;
         EVP_CIPHER* evp_aes256cbc;
 
-    private:
-        // ...
+        std::ostream& cout() { return isLogging ? std::cout : nullStream; };
+        std::ostream& cerr() { return isLogging ? std::cerr : nullStream; };
+        std::ostream nullStream{nullptr};
+
+        void keyToFile();
 };
+
+#endif
 
